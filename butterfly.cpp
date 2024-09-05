d1b2cb97f7e139356685df0820587d5b5f2b93f5
Shah Saad <shahsaaddotani@gmail.com>
2024-09-05 05:07:40 -0700

    Add files via upload

---
+++ butterfly.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int size;
+	int i=1;
+	int j=1;
+	int min_lmt=1;
+	
+	
+	cout<<"Enter the size of the butterfly:";
+	cin>>size;
+	
+		if((size%2) == 1)
+	{
+		size += 1;
+	}
+	
+	size *= 2;	
+	
+	
+	int max_lmt = size;	
+	while(i <= size) //for changing the number
+	{
+		
+		for(j=1 ;j<=size;j++) //for drawing buttterfly horitzotally each line
+		{
+			if(j<=min_lmt)
+			{
+				cout<<"+";
+			}
+			else if(j >= max_lmt)
+			{
+				cout<<"+";
+			}
+			else
+			{
+				cout<<" ";
+			}
+
+		}
+		
+	if(i < (size/2))
+	{
+		min_lmt++;
+		max_lmt--;
+	}
+	else
+	{
+		min_lmt--;
+		max_lmt++;
+	}
+	
+		
+	cout<<endl;
+	i++;
+	}
+
+return 0;
+}
